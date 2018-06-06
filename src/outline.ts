93515dbcf73c5cec3e3cc8b943ccd64969470cc7
Sebastián Gurin <sebastigurin@gmail.com>
2018-06-06 02:13:21 -0300

    clean up

--- src/outline.ts
+++ src/outline.ts
@@ -1,15 +1,12 @@
 import * as vscode from 'vscode';
-// import * as json from 'jsonc-parser';
 import * as path from 'path';
-// import { isNumber } from 'util';
 
 export class JsonOutlineProvider implements vscode.TreeDataProvider<number> {
 
 	private _onDidChangeTreeData: vscode.EventEmitter<number | null> = new vscode.EventEmitter<number | null>();
 	readonly onDidChangeTreeData: vscode.Event<number | null> = this._onDidChangeTreeData.event;
 
-	// private tree: json.Node;
-	private text: string;
+	// private text: string;
 	private editor: vscode.TextEditor;
 	private autoRefresh: boolean = true;
 
@@ -17,9 +14,9 @@ export class JsonOutlineProvider implements vscode.TreeDataProvider<number> {
 		vscode.window.onDidChangeActiveTextEditor(() => this.onActiveEditorChanged());
 		vscode.workspace.onDidChangeTextDocument(e => this.onDocumentChanged(e));
 		this.parseTree();
-		this.autoRefresh = true//vscode.workspace.getConfiguration('jsonOutline').get('autorefresh');
+		this.autoRefresh = vscode.workspace.getConfiguration('jsonOutline').get('autorefresh');
 		vscode.workspace.onDidChangeConfiguration(() => {
-			this.autoRefresh = true//vscode.workspace.getConfiguration('jsonOutline').get('autorefresh');
+			this.autoRefresh = vscode.workspace.getConfiguration('jsonOutline').get('autorefresh');
 		});
 		this.onActiveEditorChanged();
 	}
@@ -58,7 +55,8 @@ export class JsonOutlineProvider implements vscode.TreeDataProvider<number> {
 		// debugger;
 		if (vscode.window.activeTextEditor) {
 			if (vscode.window.activeTextEditor.document.uri.scheme === 'file') {
-				const enabled = vscode.window.activeTextEditor.document.languageId === 'json' || vscode.window.activeTextEditor.document.languageId === 'jsonc';
+				// debugger
+				const enabled = vscode.window.activeTextEditor.document.languageId === 'typescript' || vscode.window.activeTextEditor.document.languageId === 'javascript';
 				vscode.commands.executeCommand('setContext', 'jsonOutlineEnabled', enabled);
 				if (enabled) {
 					this.refresh();
@@ -83,18 +81,18 @@ export class JsonOutlineProvider implements vscode.TreeDataProvider<number> {
 	}
 
 	private parseTree(): void {
-		this.text = '';
+		// this.text = '';
 		// this.tree = null;
 		this.editor = vscode.window.activeTextEditor;
 		if (this.editor && this.editor.document) {
-			this.text = this.editor.document.getText();
+			// this.text = this.editor.document.getText();
 			// debugger;
 			// this.tree = json.parseTree(this.text);
 		}
 	}
 
 	getChildren(offset?: number): Thenable<number[]> {
-		debugger;
+		// debugger;
 		// if (offset) {
 		// 	const path = json.getLocation(this.text, offset).path
 		// 	const node = json.findNodeAtLocation(this.tree, path);
@@ -111,85 +109,11 @@ export class JsonOutlineProvider implements vscode.TreeDataProvider<number> {
 		return {
 			label: 'seba'+offset+this.counter++, id: 'seba'+offset+this.counter++
 		}
-		// const path = json.getLocation(this.text, offset).path
-		// const valueNode = json.findNodeAtLocation(this.tree, path);
-		// if (valueNode) {
-		// 	let hasChildren = valueNode.type === 'object' || valueNode.type === 'array';
-		// 	let treeItem: vscode.TreeItem = new vscode.TreeItem(this.getLabel(valueNode), hasChildren ? valueNode.type === 'object' ? vscode.TreeItemCollapsibleState.Expanded : vscode.TreeItemCollapsibleState.Collapsed : vscode.TreeItemCollapsibleState.None);
-		// 	treeItem.command = {
-		// 		command: 'extension.openJsonSelection',
-		// 		title: '',
-		// 		arguments: [new vscode.Range(this.editor.document.positionAt(valueNode.offset), this.editor.document.positionAt(valueNode.offset + valueNode.length))]
-		// 	};
-		// 	treeItem.iconPath = this.getIcon(valueNode);
-		// 	treeItem.contextValue = valueNode.type;
-		// 	return treeItem;
-		// }
-		// return null;
 	}
 
-	select(range: vscode.Range) {
-		this.editor.selection = new vscode.Selection(range.start, range.end);
-		debugger
-	}
-
-	// private getChildrenOffsets(node: json.Node): number[] {
-		// const offsets: number[] = [];
-		// for (const child of node.children) {
-		// 	const childPath = json.getLocation(this.text, child.offset).path
-		// 	const childNode = json.findNodeAtLocation(this.tree, childPath);
-		// 	if (childNode) {
-		// 		offsets.push(childNode.offset);
-		// 	}
-		// }
-		// return offsets;
-	// }
-	// private getIcon(node: json.Node): any {
-	// 	let nodeType = node.type;
-	// 	if (nodeType === 'boolean') {
-	// 		return {
-	// 			light: this.context.asAbsolutePath(path.join('resources', 'light', 'boolean.svg')),
-	// 			dark: this.context.asAbsolutePath(path.join('resources', 'dark', 'boolean.svg'))
-	// 		}
-	// 	}
-	// 	if (nodeType === 'string') {
-	// 		return {
-	// 			light: this.context.asAbsolutePath(path.join('resources', 'light', 'string.svg')),
-	// 			dark: this.context.asAbsolutePath(path.join('resources', 'dark', 'string.svg'))
-	// 		}
-	// 	}
-	// 	if (nodeType === 'number') {
-	// 		return {
-	// 			light: this.context.asAbsolutePath(path.join('resources', 'light', 'number.svg')),
-	// 			dark: this.context.asAbsolutePath(path.join('resources', 'dark', 'number.svg'))
-	// 		}
-	// 	}
-	// 	return null;
+	// select(range: vscode.Range) {
+	// 	this.editor.selection = new vscode.Selection(range.start, range.end);
+	// 	debugger
 	// }
 
-	// private getLabel(node: json.Node): string {
-	// 	if (node.parent.type === 'array') {
-	// 		let prefix = node.parent.children.indexOf(node).toString();
-	// 		if (node.type === 'object') {
-	// 			return prefix + ':{ }';
-	// 		}
-	// 		if (node.type === 'array') {
-	// 			return prefix + ':[ ]';
-	// 		}
-	// 		return prefix + ':' + node.value.toString();
-	// 	}
-	// 	else {
-	// 		const property = node.parent.children[0].value.toString();
-	// 		if (node.type === 'array' || node.type === 'object') {
-	// 			if (node.type === 'object') {
-	// 				return '{ } ' + property;
-	// 			}
-	// 			if (node.type === 'array') {
-	// 				return '[ ] ' + property;
-	// 			}
-	// 		}
-	// 		const value = this.editor.document.getText(new vscode.Range(this.editor.document.positionAt(node.offset), this.editor.document.positionAt(node.offset + node.length)))
-	// 		return `${property}: ${value}`;
-	// 	}
-	// }
 }
