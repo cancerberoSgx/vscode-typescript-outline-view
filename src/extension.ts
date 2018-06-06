e095757352cc71f6243a7a889b01672fbf25ce4b
Sebastián Gurin <sebastigurin@gmail.com>
2018-06-06 02:00:33 -0300

    initial contrib

---
+++ src/extension.ts
@@ -0,0 +1,18 @@
+'use strict';
+
+import * as vscode from 'vscode';
+
+import { JsonOutlineProvider } from './outline'
+
+export function activate(context: vscode.ExtensionContext) {
+	const rootPath = vscode.workspace.rootPath;
+
+	const jsonOutlineProvider = new JsonOutlineProvider(context);
+
+	vscode.window.registerTreeDataProvider('jsonOutline', jsonOutlineProvider);
+	vscode.commands.registerCommand('jsonOutline.refresh', () => jsonOutlineProvider.refresh());
+	vscode.commands.registerCommand('jsonOutline.refreshNode', offset => jsonOutlineProvider.refresh(offset));
+	vscode.commands.registerCommand('jsonOutline.renameNode', offset => jsonOutlineProvider.rename(offset));
+	vscode.commands.registerCommand('extension.openJsonSelection', range => jsonOutlineProvider.select(range));
+
+}
